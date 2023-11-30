5a79c7d36722e0c0ff128f01b8dbb18faba875ef
Nikita Shulga <shulga.nikita@work-business.ru>
2023-11-30 10:48:10 +0300

    Added battleship class

---
+++ battleship.cpp
@@ -0,0 +1,3 @@
+#include "battleship.h"
+
+battleship::battleship() {}
---
+++ battleship.h
@@ -0,0 +1,10 @@
+#ifndef BATTLESHIP_H
+#define BATTLESHIP_H
+
+class battleship
+{
+public:
+    battleship();
+};
+
+#endif // BATTLESHIP_H
