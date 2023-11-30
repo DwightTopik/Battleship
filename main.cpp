095b8e4683bdd2dc91d27bfb602a6b23c98e79b6
Nikita Shulga <shulga.nikita@work-business.ru>
2023-11-30 10:58:10 +0300

    Added reading of the file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <QCoreApplication>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+int main() {
+    const QString filePath = "/home/topik/QtProjects/BattleshipConsole/battle_ship.txt";
+    battleship game(filePath);
+    game.play();
+    return 0;
 }
